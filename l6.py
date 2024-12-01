c5e8b1e007ce5cd7e84c82a83e07e47eacbd4355
Arpit Singhal <arpitsinghal4002@gmail.com>
2024-12-01 13:11:54 +0530

    day6

---
+++ l6.py
@@ -0,0 +1,13 @@
+a = 5
+b = 5.5
+c = complex(6, 7) #for complex number
+d = "arpit"
+e = True
+f = False
+print(type(a), a) # type function returns the datatype of thew variable
+print(type(b), b)
+print(type(c), c)
+print(type(d), d)
+print(type(e), e)
+print(type(f), f)
+print(a + c)
