924036b72028aa7d536799ec260f7b24bbef024d
Arpit Singhal <arpitsinghal4002@gmail.com>
2024-12-03 19:45:41 +0530

    day11

---
+++ l11.py
@@ -0,0 +1,16 @@
+name = "arpit"
+print("hello " + name)
+
+print("my name is arpit")
+
+a = """ hello      
+    my
+    name 
+    is 
+    arpit
+    😂
+"""  #multiline string
+print(a)
+
+for i in name:
+    print(i)
