4471effc3a79ff45894e25700b0c80002ea5d623
Arpit Singhal <arpitsinghal4002@gmail.com>
2024-12-15 11:28:50 +0530

    first excersice

---
+++ codebasics/exercise1.py
@@ -0,0 +1,12 @@
+# question 1
+# height = float(input("Enter the height of the triangle: "))
+# base = float(input("Enter the base of the triangle: "))
+# area = 0.5 * height * base
+# round(area,2)
+# print(area)
+
+#question2
+
+file_name = input("Enter the file name with extensions: ")
+# print(file_name[:-3])
+print(file_name[:len(file_name)-4])
