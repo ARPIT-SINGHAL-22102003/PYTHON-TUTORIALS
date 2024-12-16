63a53032d070705932e04fe14639a9034495e9a3
Arpit Singhal <arpitsinghal4002@gmail.com>
2024-12-16 18:46:51 +0530

    exercise5

---
+++ codebasics/exercise5.py
@@ -0,0 +1,108 @@
+# question 1
+# population = {
+#     'china': 143,
+#     'india': 136,
+#     'usa': 32,
+#     'pakistan': 21
+# }
+
+# def add():
+#     country=input("Enter country name to add:")
+#     country=country.lower()
+#     if country in population:
+#         print("Country already exist in our dataset. Terminating")
+#         return
+#     p=input(f"Enter population for {country}")
+#     p=float(p)
+#     population[country]=p # Adds new key value pair to dictionary
+#     print_all()
+
+# def remove():
+#     country = input("Enter country name to remove:")
+#     country = country.lower()
+#     if country not in population:
+#         print("Country doesn't exist in our dataset. Terminating")
+#         return
+#     del population[country]
+#     print_all()
+
+# def query():
+#     country = input("Enter country name to query:")
+#     country = country.lower()
+#     if country not in population:
+#         print("Country doesn't exist in our dataset. Terminating")
+#         return
+#     print(f"Population of {country} is: {population[country]} crore")
+
+# def print_all():
+#     for country, p in population.items():
+#         print(f"{country}==>{p}")
+
+# def main():
+#     op=input("Enter operation (add, remove, query or print):")
+#     if op.lower() == 'add':
+#         add()
+#     elif op.lower() == 'remove':
+#         remove()
+#     elif op.lower() == 'query':
+#         query()
+#     elif op.lower() == 'print':
+#         print_all()
+
+# if __name__ == '__main__':
+#     main()
+
+#question 2 
+
+# import statistics
+
+# stocks = {
+#     'info': [600,630,620],
+#     'ril': [1430,1490,1567],
+#     'mtl': [234,180,160]
+# }
+
+# def print_all():
+#     for stock,price_list in stocks.items():
+#         avg = statistics.mean(price_list)
+#         print(f"{stock} ==> {price_list} ==> avg: ",round(avg,2))
+
+
+# def add():
+#     s = input("Enter a stock ticker to add:")
+#     p = input("Enter price of this stock:")
+#     p=float(p)
+#     if s in stocks:
+#         stocks[s].append(p)
+#     else:
+#         stocks[s] = [p]
+#     print_all()
+
+
+# def main():
+#     op=input("Enter operation (print, add or amend):")
+#     if op.lower() == 'print':
+#         print_all()
+#     elif op.lower() == 'add':
+#         add()
+#     else:
+#         print("Unsupported operation:",op)
+
+# if __name__ == '__main__':
+#     main()
+
+
+#question 3
+
+import math
+
+def circle_calc(r):
+    area = math.pi * r * r
+    circumference = 2 * math.pi * r
+    diameter = 2 * r
+    return area, circumference, diameter
+
+if __name__ == '__main__':
+    radius = float(input("Enter the radius: "))
+    area, circumference, diameter = circle_calc(radius)
+    print(f"area = {area} \n circumference = {circumference} \n diameter = {diameter}")
